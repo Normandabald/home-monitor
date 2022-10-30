afb7e0f9e93fb103acb3b87b0ed9b38ccebd240c
Normandabald <lpassingham@hotmail.co.uk>
2022-10-30 19:06:40 +0000

    Added support for UK river level data

--- riverlevel/scripts/riverlevels.py
+++ riverlevel/scripts/riverlevels.py
@@ -1,1 +1,31 @@
-https://environment.data.gov.uk/flood-monitoring/id/measures/E2814-level-stage-i-15_min-mASD
+import os
+import requests
+import time
+import datetime
+from prometheus_client import start_http_server, Gauge
+
+water_level = Gauge('water_level', 'Water level of river')
+water_level_timestamp = Gauge('water_level_timestamp', 'Time when last reading was taken')
+station_id = "E2814"
+
+def riverlevels():
+    response = requests.get("https://environment.data.gov.uk/flood-monitoring/id/measures/"+str(station_id)+"-level-stage-i-15_min-mASD")
+
+    # Set the Exporter value
+    water_level.set(response.json()['items']['latestReading']['value'])
+
+    reading_taken = response.json()['items']['latestReading']['dateTime']
+    # Convert DateTime Str to unix timestamp
+    date_time_obj = datetime.datetime.strptime(reading_taken, '%Y-%m-%dT%H:%M:%SZ')
+    timestamp = (date_time_obj.timestamp())
+
+    water_level_timestamp.set(timestamp)
+
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics.
+    start_http_server(8001)
+    # Periodically get river level data
+    while True:
+	    riverlevels()
+	    time.sleep(100)
