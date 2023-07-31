b10f95b1ededbcf4a5255050d012371d16afe867
Normandabald <lpassingham@hotmail.co.uk>
2023-07-31 21:08:51 +0100

    updated logging

--- riverlevel/scripts/riverlevels.py
+++ riverlevel/scripts/riverlevels.py
@@ -4,18 +4,21 @@ import time
 import datetime
 from prometheus_client import start_http_server, Gauge
 from dotenv import load_dotenv
-load_dotenv()
+import logging
 
-print("Starting container...")
+load_dotenv()
+logging.basicConfig(filename='riverlevels.log', level=logging.DEBUG)
+logging.info('Starting riverlevels exporter')
 water_level = Gauge('water_level', 'Water level of river')
 water_level_timestamp = Gauge('water_level_timestamp', 'Time when last reading was taken')
 station_id = os.getenv('RIVER_STATION_ID')
-# station_id = "E2814"
-print("Starting to monitor river levels for station: "+str('STATION_ID'))
+
+
 def check_riverlevels():
+    """Calls the river level API and returns the latest reading"""
     try:
         response = requests.get("https://environment.data.gov.uk/flood-monitoring/id/measures/"+str(station_id)+"-level-stage-i-15_min-mASD")
-    # Set the Exporter value
+        # Set the Exporter value
         water_level.set(response.json()['items']['latestReading']['value'])
 
         reading_taken = response.json()['items']['latestReading']['dateTime']
@@ -24,15 +27,14 @@ def check_riverlevels():
         timestamp = (date_time_obj.timestamp())
 
         water_level_timestamp.set(timestamp)
-        print("River level is: "+str(response.json()['items']['latestReading']['value'])+" at: "+str(reading_taken))
+        logging.info('River level is: %s at: %s', response.json()['items']['latestReading']['value'], reading_taken)
     except Exception as e:
-        print(e)
-
+        logging.error('Error getting river level: %s', e)
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8001)
     # Periodically get river level data
     while True:
-	    check_riverlevels()
-	    time.sleep(100)
+        check_riverlevels()
+        time.sleep(100)
--- speedtest/scripts/test_connection.py
+++ speedtest/scripts/test_connection.py
@@ -1,3 +1,4 @@
+import logging
 import time
 from prometheus_client import start_http_server, Gauge
 import speedtest
@@ -6,25 +7,28 @@ Download = Gauge('download_speed', 'Download Speed')
 Upload = Gauge('upload_speed', 'Upload Speed')
 Ping = Gauge('ping', 'Ping')
 
-def testSpeed():
-	"""Calls the speedtest API and returns Download, Upload and Ping"""
-	servers = []
-	threads = None
+logging.basicConfig(filename='test_connection.log', level=logging.DEBUG)
 
-	try:
-		s = speedtest.Speedtest()
-		s.get_servers(servers)
-		s.get_best_server()
-		Download.set(s.download(threads=threads))
-		Upload.set(s.upload(threads=threads))
-		Ping.set(s.results.ping)
-	except Exception as e:
-		print(e)
+def testSpeed():
+    """Calls the speedtest API and returns Download, Upload and Ping"""
+    servers = []
+    threads = None
+
+    try:
+        s = speedtest.Speedtest()
+        s.get_servers(servers)
+        s.get_best_server()
+        Download.set(s.download(threads=threads))
+        Upload.set(s.upload(threads=threads))
+        Ping.set(s.results.ping)
+        logging.info('Speedtest successful')
+    except Exception as e:
+        logging.error('Speedtest failed: %s', e)
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8000)
     # Generate some requests.
     while True:
-	    testSpeed()
-	    time.sleep(60)
+        testSpeed()
+        time.sleep(60)
